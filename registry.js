d2ecb1dedd704f3451eedb1c4ed850e1725d5b36
Georgii Ivankin <semper.cras@gmail.com>
2014-12-09 12:22:15 +0400

    Sum up changes, followup

--- registry.js
+++ registry.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var debug = require('debug')('licenses::npm');
+var readJson = require('../read-package-json');
 
 /**
  * Parser for npm based license information.
@@ -20,30 +21,59 @@ module.exports = require('./parser').extend({
   /**
    * Parse the npm license information from the package.
    *
-   * @param {Object} data The package.json or npm package contents.
+   * @param {Object} dep The package.json or npm package contents.
    * @param {Object} options Optional options.
    * @param {Function} next Continuation.
    * @api public
    */
-  parse: function parse(data, options, next) {
-    data = this.get(data);
+  parse: function parse(dep, options, next) {
+    var parser = this;
 
     if ('function' === typeof options) {
       next = options;
       options = {};
     }
 
+    var matches = [];
+    var data = dep.data;
+    //
+    // Another npm oddity, it allows licenses to be specified in to different
+    // properties. Because why the fuck not?
+    //
+    ['license', 'licenses'].forEach(function each(key) {
+      if ('string' === typeof data[key]) {
+        return matches.push(data[key]);
+      }
+
+      if (Array.isArray(data[key])) {
+        return Array.prototype.push.apply(
+          matches,
+          data[key].map(function map(item) {
+            return parser.license(item);
+          }).filter(Boolean)
+        );
+      }
+
+      if ('object' === typeof data[key] && parser.license(data[key])) {
+        return Array.prototype.push.apply(
+          matches,
+          [parser.license(data[key])]
+        );
+      }
+    });
+
     //
     // We cannot detect a license so we call the callback without any arguments
     // which symbolises a failed attempt.
     //
-    if (!data) return next();
+    if (!matches.length) return next();
 
-    debug('found %s in the package contents', data);
+    debug('found %s in the package contents', matches);
 
     // @TODO handle the edge case where people give us an URL instead of an
     // actual license.
-    next(undefined, this.normalize(data));
+    next(undefined, parser.normalize(matches));
+
   },
 
   /**
@@ -73,7 +103,7 @@ module.exports = require('./parser').extend({
    * @api public
    */
   supported: function supported(data) {
-    return !!this.get(data);
+    return true;
   },
 
   /**
@@ -87,31 +117,7 @@ module.exports = require('./parser').extend({
     var parser = this
       , matches = [];
 
-    //
-    // Another npm oddity, it allows licenses to be specified in to different
-    // properties. Because why the fuck not?
-    //
-    ['license', 'licenses'].forEach(function each(key) {
-      if ('string' === typeof data[key]) {
-        return matches.push(data[key]);
-      }
 
-      if (Array.isArray(data[key])) {
-        return Array.prototype.push.apply(
-          matches,
-          data[key].map(function map(item) {
-            return parser.license(item);
-          }).filter(Boolean)
-        );
-      }
-
-      if ('object' === typeof data[key] && parser.license(data[key])) {
-        return Array.prototype.push.apply(
-          matches,
-          [parser.license(data[key])]
-        );
-      }
-    });
 
     if (matches.length) return matches;
   }
